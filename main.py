476bb3cb2cd1fee95b3e02074685bc84714e7654
alexgouna <85231687+alexgouna@users.noreply.github.com>
2023-03-13 11:39:14 +0200

    Ver 1.0

--- main.py
+++ main.py
@@ -9,18 +9,19 @@ def millioner():
     global image_num
     global timer
     global lbl_timer
-    global btnanswera
-    global btnanswerb
-    global btnanswerc
-    global btnanswerd
+    global lbl_help2_answer
+    global btn_answer_a
+    global btn_answer_b
+    global btn_answer_c
+    global btn_answer_d
     global question
     global questions_easy
     global questions_medium
     global questions_hard
     global position
-    global btnhelp1
-    global btnhelp2
-    global btnhelp3
+    global btn_help_1
+    global btn_help_2
+    global btn_help_3
     global total_time
     global kerdismeno_poso
 
@@ -111,20 +112,24 @@ def millioner():
         global question
         global position
         global kerdismeno_poso
+        global lbl_help2_answer
         print("correct!! go to next...")
         next_amount()
         new_question()
         kerdismeno_poso = position * 10
         position = position + 1
-        lblquestion.config(text=question[0])
-        btnanswera.config(text=question[1], state=NORMAL)
-        btnanswerb.config(text=question[2], state=NORMAL)
-        btnanswerc.config(text=question[3], state=NORMAL)
-        btnanswerd.config(text=question[4], state=NORMAL)
+        lbl_question.config(text=question[0])
+        btn_answer_a.config(text=question[1], state=NORMAL)
+        btn_answer_b.config(text=question[2], state=NORMAL)
+        btn_answer_c.config(text=question[3], state=NORMAL)
+        btn_answer_d.config(text=question[4], state=NORMAL)
+        lbl_timer.config(padx=211)
+        lbl_help2_answer.destroy()
 
     def give_answer(answer):
         global timer
         global total_time
+        global lbl_help2_answer
 
         if position == 15:
             game_over()
@@ -138,11 +143,11 @@ def millioner():
 
     # help 50/50
     def help1():
-        global btnhelp1
-        global btnanswera
-        global btnanswerb
-        global btnanswerc
-        global btnanswerd
+        global btn_help_1
+        global btn_answer_a
+        global btn_answer_b
+        global btn_answer_c
+        global btn_answer_d
 
         if question[5] == "A":
             num1 = 1
@@ -158,14 +163,14 @@ def millioner():
             num2 = random.choice([1, 2, 3])
         print(num1, num2)
         if not (num1 == 1 or num2 == 1):
-            btnanswera.config(state=DISABLED)
+            btn_answer_a.config(state=DISABLED)
         if not (num1 == 2 or num2 == 2):
-            btnanswerb.config(state=DISABLED)
+            btn_answer_b.config(state=DISABLED)
         if not (num1 == 3 or num2 == 3):
-            btnanswerc.config(state=DISABLED)
+            btn_answer_c.config(state=DISABLED)
         if not (num1 == 4 or num2 == 4):
-            btnanswerd.config(state=DISABLED)
-        btnhelp1.config(command="", state=DISABLED)
+            btn_answer_d.config(state=DISABLED)
+        btn_help_1.config(command="", state=DISABLED)
 
     def pc_answer(bonus):
         num = (random.random(), random.random(), random.random(), random.random())
@@ -193,9 +198,10 @@ def millioner():
             max[1] = "D"
         return max
 
-    # help propose an answer
+    # help2 PC propose an answer
     def help2():
-        global btnhelp2
+        global btn_help_2
+        global lbl_help2_answer
 
         print("help2")
         if position <= 5:
@@ -205,38 +211,37 @@ def millioner():
         else:
             my_pc_answer = pc_answer(15)
 
-        root_help2 = Toplevel()
-        root_help2.title = "Ποσοστό κοινού"
-        root_help2.geometry("300x100")
-        lbl_mylabel = Label(root_help2,text="Με ποσοστό " + str(int(my_pc_answer[0])) + "% δίνεται η απάντηση: \n" + str(my_pc_answer[1]))
-        lbl_mylabel.pack()
-        mainloop()
-        btnhelp2.config(state=DISABLED)
+        lbl_help2_answer = Label(mid_l_frame , text="Με ποσοστό " + str(int(my_pc_answer[0])) + "% δίνεται η απάντηση: \n" + str(my_pc_answer[1]),
+                            font=("Arial", 10), width= 30, pady=30, bg="black", fg="white")
+        lbl_help2_answer.grid(row=0,column=1)
+        lbl_timer.config(padx=88)
+        btn_help_2.config(state=DISABLED)
 
     def help3():
-        global btnhelp3
+        global btn_help_3
 
         print("help3")
         new_question()
-        lblquestion.config(text=question[0])
-        btnanswera.config(text=question[1], state=NORMAL)
-        btnanswerb.config(text=question[2], state=NORMAL)
-        btnanswerc.config(text=question[3], state=NORMAL)
-        btnanswerd.config(text=question[4], state=NORMAL)
-        btnhelp3.config(command="", state=DISABLED)
+        lbl_question.config(text=question[0])
+        btn_answer_a.config(text=question[1], state=NORMAL)
+        btn_answer_b.config(text=question[2], state=NORMAL)
+        btn_answer_c.config(text=question[3], state=NORMAL)
+        btn_answer_d.config(text=question[4], state=NORMAL)
+        btn_help_3.config(state=DISABLED)
 
     def start_game():
         global lbl_timer
         global timer
+        global lbl_help2_answer
 
-        lblquestion.place(x=100, y=100)
-        btnanswera.place(x=100, y=200)
-        btnanswerb.place(x=100, y=300)
-        btnanswerc.place(x=200, y=200)
-        btnanswerd.place(x=200, y=300)
-        btnhelp1.config(state=NORMAL)
-        btnhelp2.config(state=NORMAL)
-        btnhelp3.config(state=NORMAL)
+        lbl_question.place(x=100, y=100)
+        btn_answer_a.place(x=100, y=200)
+        btn_answer_b.place(x=100, y=300)
+        btn_answer_c.place(x=200, y=200)
+        btn_answer_d.place(x=200, y=300)
+        btn_help_1.config(state=NORMAL)
+        btn_help_2.config(state=NORMAL)
+        btn_help_3.config(state=NORMAL)
 
         while True:
             if timer > 0:
@@ -256,63 +261,63 @@ def millioner():
     top_root.title("Millioner")
     top_root.config(bg="black")
     # top_root.resizable(False, False)
-    lframe = Frame(top_root)
-    rframe = Frame(top_root)
-    lframe.grid(row=0, column=0)
-    rframe.grid(row=0, column=1)
+    l_frame = Frame(top_root)
+    r_frame = Frame(top_root)
+    l_frame.grid(row=0, column=0)
+    r_frame.grid(row=0, column=1)
 
-    # lframe
-    toplframe = Frame(lframe, bg="black")
-    midlframe = Frame(lframe, bg="blue")
-    botlframe = Frame(lframe, bg="red")
-    toplframe.grid(row=0, column=0)
-    midlframe.grid(row=1, column=0)
-    botlframe.grid(row=2, column=0)
+    # l_frame
+    top_l_frame = Frame(l_frame, bg="black")
+    mid_l_frame = Frame(l_frame, bg="blue")
+    bot_l_frame = Frame(l_frame, bg="red")
+    top_l_frame.grid(row=0, column=0)
+    mid_l_frame.grid(row=1, column=0)
+    bot_l_frame.grid(row=2, column=0)
 
-    # toplframe
-    lbl_bg_top_l = Label(toplframe, bg="black", text="", padx=248, pady=40)
+    # top_l_frame
+    lbl_bg_top_l = Label(top_l_frame, bg="black", text="", padx=248, pady=40)
     lbl_bg_top_l.grid(row=0, column=0, columnspan=3)
-    btnhelp1 = Button(toplframe, text="50", bg="red", bd=0, activebackground="black", padx=50, pady=20,
+    btn_help_1 = Button(top_l_frame, text="50", bg="red", bd=0, activebackground="black", padx=50, pady=20,
                       command=help1, state=DISABLED)
-    btnhelp1.grid(row=0, column=0)
-    btnhelp2 = Button(toplframe, text="percent", bg="red", bd=0, activebackground="black", padx=50, pady=20,
+    btn_help_1.grid(row=0, column=0)
+    btn_help_2 = Button(top_l_frame, text="percent", bg="red", bd=0, activebackground="black", padx=50, pady=20,
                       command=help2, state=DISABLED)
-    btnhelp2.grid(row=0, column=1)
-    btnhelp3 = Button(toplframe, text="change", bg="red", bd=0, activebackground="black", padx=50, pady=20,
+    btn_help_2.grid(row=0, column=1)
+    btn_help_3 = Button(top_l_frame, text="change", bg="red", bd=0, activebackground="black", padx=50, pady=20,
                       command=help3, state=DISABLED)
-    btnhelp3.grid(row=0, column=2)
+    btn_help_3.grid(row=0, column=2)
 
-    # midlframe
-    midlframe.config(width=500, height=150)
-    lbl_timer = Label(midlframe, text="00", font=("Arial", 50), padx=211, pady=30, bg="black", fg="white")
+    # mid_l_frame
+    mid_l_frame.config(width=500, height=150)
+    lbl_timer = Label(mid_l_frame, text="00", font=("Arial", 50), padx=211, pady=30, bg="black", fg="white")
     lbl_timer.grid(row=0, column=0)
 
-    # botlframe
-    botlframe.config(width=500, height=350)
-    lblquestion = Label(botlframe, text=question[0], bg="black", fg="white")
-    btnanswera = Button(botlframe, text=question[1], bg="black", fg="white", command=lambda: give_answer("A"))
-    btnanswerb = Button(botlframe, text=question[2], bg="black", fg="white", command=lambda: give_answer("B"))
-    btnanswerc = Button(botlframe, text=question[3], bg="black", fg="white", command=lambda: give_answer("C"))
-    btnanswerd = Button(botlframe, text=question[4], bg="black", fg="white", command=lambda: give_answer("D"))
+    # bot_l_frame
+    bot_l_frame.config(width=500, height=350)
+    lbl_question = Label(bot_l_frame, text=question[0], bg="black", fg="white")
+    btn_answer_a = Button(bot_l_frame, text=question[1], bg="black", fg="white", command=lambda: give_answer("A"))
+    btn_answer_b = Button(bot_l_frame, text=question[2], bg="black", fg="white", command=lambda: give_answer("B"))
+    btn_answer_c = Button(bot_l_frame, text=question[3], bg="black", fg="white", command=lambda: give_answer("C"))
+    btn_answer_d = Button(bot_l_frame, text=question[4], bg="black", fg="white", command=lambda: give_answer("D"))
 
-    # rframe
+    # r_frame
     padx = 145
     pady = 290
-    label1 = Label(rframe, text="1", padx=padx, pady=pady, bg="blue")
-    label2 = Label(rframe, text="2", padx=padx, pady=pady, bg="blue")
-    label3 = Label(rframe, text="3", padx=padx, pady=pady, bg="blue")
-    label4 = Label(rframe, text="4", padx=padx, pady=pady, bg="blue")
-    label5 = Label(rframe, text="5", padx=padx, pady=pady, bg="blue")
-    label6 = Label(rframe, text="6", padx=padx, pady=pady, bg="blue")
-    label7 = Label(rframe, text="7", padx=padx, pady=pady, bg="blue")
-    label8 = Label(rframe, text="8", padx=padx, pady=pady, bg="blue")
-    label9 = Label(rframe, text="9", padx=padx, pady=pady, bg="blue")
-    label10 = Label(rframe, text="10", padx=padx, pady=pady, bg="blue")
-    label11 = Label(rframe, text="11", padx=padx, pady=pady, bg="blue")
-    label12 = Label(rframe, text="12", padx=padx, pady=pady, bg="blue")
-    label13 = Label(rframe, text="13", padx=padx, pady=pady, bg="blue")
-    label14 = Label(rframe, text="14", padx=padx, pady=pady, bg="blue")
-    label15 = Label(rframe, text="15", padx=padx, pady=pady, bg="blue")
+    label1 = Label(r_frame, text="1", padx=padx, pady=pady, bg="blue")
+    label2 = Label(r_frame, text="2", padx=padx, pady=pady, bg="blue")
+    label3 = Label(r_frame, text="3", padx=padx, pady=pady, bg="blue")
+    label4 = Label(r_frame, text="4", padx=padx, pady=pady, bg="blue")
+    label5 = Label(r_frame, text="5", padx=padx, pady=pady, bg="blue")
+    label6 = Label(r_frame, text="6", padx=padx, pady=pady, bg="blue")
+    label7 = Label(r_frame, text="7", padx=padx, pady=pady, bg="blue")
+    label8 = Label(r_frame, text="8", padx=padx, pady=pady, bg="blue")
+    label9 = Label(r_frame, text="9", padx=padx, pady=pady, bg="blue")
+    label10 = Label(r_frame, text="10", padx=padx, pady=pady, bg="blue")
+    label11 = Label(r_frame, text="11", padx=padx, pady=pady, bg="blue")
+    label12 = Label(r_frame, text="12", padx=padx, pady=pady, bg="blue")
+    label13 = Label(r_frame, text="13", padx=padx, pady=pady, bg="blue")
+    label14 = Label(r_frame, text="14", padx=padx, pady=pady, bg="blue")
+    label15 = Label(r_frame, text="15", padx=padx, pady=pady, bg="blue")
 
     label_all = [label1, label2, label3, label4, label5, label6, label7, label8, label9, label10, label11, label12,
                  label13, label14, label15]
