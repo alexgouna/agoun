159d74cf76e372e77cd95c3d3be74506d94a73af
Alex <agounaropoulos@gmail.com>
2023-05-25 19:32:55 +0300

    v2.2

--- rank.py
+++ rank.py
@@ -8,33 +8,33 @@ from PIL import ImageTk, Image
 import settings
 global my_selection_question
 
-
-
 def new_rank(counter):
     toplevel_rank = Toplevel()
     toplevel_rank.geometry("400x400")
     toplevel_rank.title("Main")
 
     settings.counter_toplevel_rank = 1
+    settings.counter_game = 0
 
     def close():
         settings.counter_toplevel_rank = 0
         toplevel_rank.destroy()
     toplevel_rank.protocol("WM_DELETE_WINDOW", close)
 
-    def submit():
+    def submit(name,counter):
         conn = sqlite3.connect('millionerdb.db')
         c = conn.cursor()
-        c.execute(f"INSERT INTO rank_table (name,score) VALUES ('{e.get()}','{score}')")
+        c.execute(f"INSERT INTO rank_table (name,score) VALUES ('{name}','{counter}')")
         conn.commit()
         conn.close()
+        close()
 
 
     lbl_main = Label(toplevel_rank,text=f"Congratulation!! \nYour score is {counter}")
     lbl_name = Label(toplevel_rank,text="Name")
     e = Entry(toplevel_rank)
     e.insert(0, "")
-    btn_submit = Button(toplevel_rank, text="Submit", command=lambda :submit(e.get(),counter))
+    btn_submit = Button(toplevel_rank, text="Submit", command=lambda:submit(e.get(), counter))
 
     lbl_main.grid(row=0, column=0, columnspan=2)
     lbl_name.grid(row=1,column=0)
