982467fa3bc3dee8654b0f945c063b882f34a1c7
alexgouna <85231687+alexgouna@users.noreply.github.com>
2023-06-02 12:56:55 +0300

    V3.0

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ import rank
 import millioner_button
 import settings
 
+
 class MillionaireGame(tk.Toplevel):
     def __init__(self, master):
         tk.Toplevel.__init__(self, master)
@@ -48,13 +49,15 @@ class MillionaireGame(tk.Toplevel):
         self.stop_game()
 
     def stop_game(self):
-        self.stop_game_button = tk.Button(self.left_top_frame, text="Stop game",fg="white",font=("Arial",30), bg="black", bd=1,command=lambda:self.game_over(self.counter))
+        self.stop_game_button = tk.Button(self.left_top_frame, text="Stop game", fg="white", font=("Arial", 30),
+                                          bg="black", bd=1, command=lambda: self.game_over(self.counter))
         self.stop_game_button.pack(side="left")
 
     def my_lives(self):
         self.left_bottom_frame = tk.Frame(self.left_frame, bg="black")
         self.left_bottom_frame.pack(side="bottom", fill="both")
-        self.live_text_label = tk.Label(self.left_bottom_frame, text="My lives", fg="red", bg="black", font=('Arial', 30))
+        self.live_text_label = tk.Label(self.left_bottom_frame, text="My lives", fg="red", bg="black",
+                                        font=('Arial', 30))
         self.live_text_label.pack(side="top")
         img = Image.open('assets/live.png')
         img = img.resize((60, 60))
@@ -63,30 +66,29 @@ class MillionaireGame(tk.Toplevel):
             self.live_label = tk.Label(self.left_bottom_frame, image=self.live_image, bg="black", bd=0)
             self.live_label.pack(side="left")
 
-
     def set_timer(self):
-        if self.counter==5 or self.counter==10 or self.counter==15:
+        if self.counter == 5 or self.counter == 10 or self.counter == 15:
             self.total_safe_time = self.total_time
+
         def update():
             self.my_timer = self.my_timer - 1
             self.total_time = self.total_time + 1
-            self.my_timer_lbl.configure(text=round(self.my_timer/100))
-            if self.my_timer >0:
+            self.my_timer_lbl.configure(text=round(self.my_timer / 100))
+            if self.my_timer > 0:
                 self.my_timer_lbl.after(10, update)
             else:
-                if self.counter<15:
+                if self.counter < 15:
                     self.game_over(10)
-                elif self.counter<15:
+                elif self.counter < 15:
                     self.game_over(5)
                 else:
                     self.game_over(0)
 
-        self.my_text_lbl = tk.Label(self.left_top_frame, text="Timer",font=('Arial', 40), bg='black', fg='white')
-        self.my_timer_lbl = tk.Label(self.left_top_frame, text="60",font=('Arial', 60), bg='black', fg='white')
+        self.my_text_lbl = tk.Label(self.left_top_frame, text="Timer", font=('Arial', 40), bg='black', fg='white')
+        self.my_timer_lbl = tk.Label(self.left_top_frame, text="60", font=('Arial', 60), bg='black', fg='white')
         self.my_text_lbl.pack(pady=20)
         self.my_timer_lbl.pack(pady=20)
         self.after(1, update)
-
 
     def setup_answers(self):
         self.main_questions_frame = tk.Frame(self.main_frame, bg="yellow")
@@ -106,21 +108,22 @@ class MillionaireGame(tk.Toplevel):
         if self.counter < 5:
             self.question = settings.questions_easy[self.counter]
         elif self.counter < 10:
-            self.question = settings.questions_medium[self.counter-5]
+            self.question = settings.questions_medium[self.counter - 5]
         elif self.counter < 15:
-            self.question = settings.questions_hard[self.counter-10]
+            self.question = settings.questions_hard[self.counter - 10]
         else:
             self.game_over(self.counter)
         self.screen_reset()
-        self.my_timer=6000
+        self.my_timer = 6000
 
-    def game_over(self,my_counter):
-        prizes = [0,25,50,100,200,300,500,1000,5000,10000,25000,50000,100000,250000,500000,1000000]
+    def game_over(self, my_counter):
+        prizes = [0, 25, 50, 100, 200, 300, 500, 1000, 5000, 10000, 25000, 50000, 100000, 250000, 500000, 1000000]
         settings.my_window = "βαθμολογίας"
         self.destroy()
-        if self.total_safe_time!=0:
-            my_score= round(prizes[my_counter]/(self.total_safe_time/100))
-            rank.new_rank(prizes[my_counter],round(self.total_safe_time/100),round(self.total_safe_time/my_counter)/100, my_score)
+        if self.total_safe_time != 0:
+            my_score = round(prizes[my_counter] / (self.total_safe_time / 100))
+            rank.new_rank(prizes[my_counter], round(self.total_safe_time / 100),
+                          round(self.total_safe_time / my_counter) / 100, my_score)
         else:
             settings.open_window = 0
 
@@ -129,10 +132,10 @@ class MillionaireGame(tk.Toplevel):
             self.counter = self.counter + 1
             self.correct_answer()
         else:
-            self.lives = self.lives -1
+            self.lives = self.lives - 1
             self.left_bottom_frame.destroy()
             self.my_lives()
-            if self.lives==0:
+            if self.lives == 0:
                 if self.counter < 15:
                     self.game_over(10)
                 elif self.counter < 15:
@@ -168,8 +171,8 @@ class MillionaireGame(tk.Toplevel):
         img = Image.open('assets/Classic5050X.png')
         img = img.resize((62, 48))
         self.button_5050_image = ImageTk.PhotoImage(img)
-        self.button_5050.config(image=self.button_5050_image,state=tk.DISABLED)
-        self.question= millioner_button.help5050(self.question)
+        self.button_5050.config(image=self.button_5050_image, state=tk.DISABLED)
+        self.question = millioner_button.help5050(self.question)
         self.screen_reset()
 
     def setup_5050_button(self):
@@ -178,16 +181,16 @@ class MillionaireGame(tk.Toplevel):
         img = Image.open('assets/Classic5050.png')
         img = img.resize((62, 48))
         self.button_5050_image = ImageTk.PhotoImage(img)
-        self.button_5050 = tk.Button(self.button_5050_frame, bg="red", image=self.button_5050_image, bd=0, command=self.help5050)
+        self.button_5050 = tk.Button(self.button_5050_frame, bg="red", image=self.button_5050_image, bd=0,
+                                     command=self.help5050)
         self.button_5050.pack(fill="both", expand=True)
 
     def helppc(self):
         img = Image.open('assets/ClassicATAx.png')
         img = img.resize((62, 48))
         self.button_ata_image = ImageTk.PhotoImage(img)
-        self.button_ata.config(image=self.button_ata_image,state=tk.DISABLED)
+        self.button_ata.config(image=self.button_ata_image, state=tk.DISABLED)
         millioner_button.helppc(self.question)
-
 
     def setup_ata_button(self):
         self.button_ata_frame = tk.Frame(self.options_frame, bg="yellow")
@@ -195,15 +198,16 @@ class MillionaireGame(tk.Toplevel):
         img = Image.open('assets/ClassicATA.png')
         img = img.resize((62, 48))
         self.button_ata_image = ImageTk.PhotoImage(img)
-        self.button_ata = tk.Button(self.button_ata_frame, bg="yellow", image=self.button_ata_image, bd=0 , command=self.helppc)
+        self.button_ata = tk.Button(self.button_ata_frame, bg="yellow", image=self.button_ata_image, bd=0,
+                                    command=self.helppc)
         self.button_ata.pack(fill="both", expand=True)
 
     def helpchange(self):
         img = Image.open('assets/ClassicREPX.png')
         img = img.resize((62, 48))
         self.button_paf_image = ImageTk.PhotoImage(img)
-        self.button_paf.config(image=self.button_paf_image,state=tk.DISABLED)
-        self.question= millioner_button.helpchange(self.question)
+        self.button_paf.config(image=self.button_paf_image, state=tk.DISABLED)
+        self.question = millioner_button.helpchange(self.question)
         self.screen_reset()
 
     def setup_paf_button(self):
@@ -212,7 +216,8 @@ class MillionaireGame(tk.Toplevel):
         img = Image.open('assets/ClassicREP.png')
         img = img.resize((62, 48))
         self.button_paf_image = ImageTk.PhotoImage(img)
-        self.button_paf = tk.Button(self.button_paf_frame, bg="purple", image=self.button_paf_image, bd=0, command=self.helpchange)
+        self.button_paf = tk.Button(self.button_paf_frame, bg="purple", image=self.button_paf_image, bd=0,
+                                    command=self.helpchange)
         self.button_paf.pack(fill="both", expand=True)
 
     def setup_sidebar_prizes(self):
@@ -227,7 +232,7 @@ class MillionaireGame(tk.Toplevel):
             if my_counter == self.counter:
                 label = tk.Label(self.prizes_frame, text=prize, bg="yellow3", fg="white", font=("Helvetica", 14))
             else:
-                if my_counter==5 or my_counter==10:
+                if my_counter == 5 or my_counter == 10:
                     label = tk.Label(self.prizes_frame, text=prize, bg="green4", fg="white", font=("Helvetica", 14))
                 else:
                     label = tk.Label(self.prizes_frame, text=prize, bg="blue", fg="white", font=("Helvetica", 14))
@@ -241,7 +246,6 @@ class MillionaireGame(tk.Toplevel):
     def close(self):
         settings.open_window = 0
         self.destroy()
-
 
 
 def start_game(root):
